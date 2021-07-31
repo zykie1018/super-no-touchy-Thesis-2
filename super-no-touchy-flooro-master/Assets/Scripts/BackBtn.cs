e2f2a113c608d293a51def33b8af9a6eb29db92b
zykier <zkerallos@addu.edu.ph>
2021-07-31 15:58:14 +0800

    pause menu script

---
+++ super-no-touchy-flooro-master/Assets/Scripts/BackBtn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackBtn : MonoBehaviour
+{
+    public GameObject pauseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Toggle();
+        }     
+    }
+
+    // Pause Menu
+    public void Toggle()
+    {
+        pauseUI.SetActive(!pauseUI.activeSelf);
+            if(pauseUI.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+    }
+}
